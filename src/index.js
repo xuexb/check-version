31d5754c9b1943d15bd258e94e2891f9c5fcefca
xuexb <fe.xiaowu@gmail.com>
2016-02-17 23:39:44 +0800

    调整文件结构

--- src/index.js
+++ src/index.js
@@ -7,110 +7,91 @@
 'use strict';
 
 import request from 'request';
-import {green, red} from 'colors/safe';
 import KeyCache from 'key-cache';
 
 import config from '../config.json';
-import header from '../header.json';
-
-let promiseAll = [];
-let rule = config.rule;
 
 let cache = new KeyCache({
     dir: './cache/',
     md5key: false
 });
 
-let lastVertion = cache.get('lastVertion') || {};
+export default () => {
+    let promiseAll = [];
 
-rule.forEach((val) => {
-    let temp = new Promise((resolve, reject) => {
-        request.get({
-            headers: header,
-            url: val.url
-        }, (error, response, body) => {
-            if (error) {
-                val.errcode = 1;
-                val.errmsg = error;
-                reject(val);
-            }
-            else if (response.statusCode !== 200) {
-                val.errcode = 2;
-                val.errmsg = response.statusCode;
-                reject(val);
-            }
-            else {
-                let reg = new RegExp(val.reg);
-                let match = val.match || '$1';
-                match = Math.floor(match.replace('$', '')) || 0;
+    let lastVertion = cache.get('lastVertion') || {};
 
-                try {
-                    body = body.match(reg);
-                    if (body && body.length > match - 1) {
-                        body = body[match];
+    config.rule.forEach((val) => {
+        let temp = new Promise((resolve, reject) => {
+            request.get({
+                headers: config.header,
+                url: val.url
+            }, (error, response, body) => {
+                if (error) {
+                    val.errcode = 1;
+                    val.errmsg = error;
+                    reject(val);
+                }
+                else if (response.statusCode !== 200) {
+                    val.errcode = 2;
+                    val.errmsg = response.statusCode;
+                    reject(val);
+                }
+                else {
+                    let reg = new RegExp(val.reg);
+                    let match = val.match || '$1';
+                    match = Math.floor(match.replace('$', '')) || 0;
+
+                    try {
+                        body = body.match(reg);
+                        if (body && body.length > match - 1) {
+                            body = body[match];
+                        }
+
+                    }
+                    catch (e) {
+                        body = null;
                     }
 
-                }
-                catch (e) {
-                    body = null;
-                }
+                    val.version = body;
 
-                val.version = body;
-
-                resolve(val);
-            }
+                    resolve(val);
+                }
+            });
         });
-    }).then(data => {
-        if (data.version) {
-            console.log(green(data.name + ' => ' + data.version));
-        }
-        else {
-            console.log(red(data.name + ' => ' + data.version));
-        }
 
-        return data;
-    }, err => {
-        console.error(red(err));
+        promiseAll.push(temp);
     });
 
-    promiseAll.push(temp);
-});
+    return Promise.all(promiseAll).then(data => {
+        let res = {
+            update: [],
+            all: data
+        };
 
-Promise.all(promiseAll).then(data => {
-    let update = [];
+        data.forEach(val => {
+            let key = val.name;
 
-    console.log('------------------------');
+            // 如果没有获取到版本
+            if (!val.version) {
+                return;
+            }
 
-    data.forEach(val => {
-        let key = val.name;
-
-        // 如果没有获取到版本
-        if (!val.version) {
-            return;
-        }
-
-        // 如果上个版本缓存过
-        if (lastVertion.hasOwnProperty(key)) {
-            if (val.version > lastVertion[key] || lastVertion[key] === null) {
-                val.lastVertion = lastVertion[key];
-                update.push(val);
+            // 如果上个版本缓存过
+            if (lastVertion.hasOwnProperty(key)) {
+                if (val.version > lastVertion[key] || lastVertion[key] === null) {
+                    val.lastVertion = lastVertion[key];
+                    res.update.push(val);
+                    lastVertion[key] = val.version;
+                }
+            }
+            else {
                 lastVertion[key] = val.version;
             }
-        }
-        else {
-            lastVertion[key] = val.version;
-        }
-    });
-
-    if (!update.length) {
-        console.log(green('没有更新'));
-    }
-    else {
-        console.log(green('有 ') + red(update.length) + green(' 个更新'));
-        update.forEach(val => {
-            console.log(green(val.name + ' ' + val.lastVertion + ' => ' + val.version));
         });
-    }
 
-    cache.set('lastVertion', lastVertion);
-});
+        cache.set('lastVertion', lastVertion);
+
+        return res;
+    });
+};
