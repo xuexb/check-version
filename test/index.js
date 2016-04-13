56033a1aa396fb23442ebd3718b24835475c1e8c
xuexb <fe.xiaowu@gmail.com>
2016-04-13 22:09:36 +0800

    update: 添加测试case

--- test/index.js
+++ test/index.js
@@ -7,10 +7,16 @@ import 'should';
 
 import Check from '../src/index';
 import types from './types';
-
+import log from '../src/log';
 
 
 describe('check-version', () => {
+    let write = log._write;
+
+    afterEach(() => {
+        log._write = write;
+    });
+
     it('new', () => {
         Check.should.be.type('function');
     });
@@ -79,4 +85,82 @@ describe('check-version', () => {
     //         done();
     //     });
     // });
+    
+    it('_sendLog promise', () => {
+        Check._sendLog().should.be.Promise();
+    });
+
+    // 没有更新
+    it('_sendLog({}, {})', done => {
+        let num = 0;
+        log._write = () => {
+            num += 1;
+        };
+        return Check._sendLog({}, {
+            update: [],
+            all: []
+        }).then(data => {
+            data.update.length.should.be.equal(0);
+            data.all.length.should.be.equal(0);
+            num.should.be.equal(2);
+        }).then(done);
+    });
+
+    it('_sendLog update:1', done => {
+        let num = 0;
+        log._write = () => {
+            num += 1;
+        };
+
+        Check._sendLog({}, {
+            update: [
+                {
+                    prevVersion: 1,
+                    name: 'test1',
+                    version: 2
+                }
+            ],
+            all: [
+                {
+                    prevVersion: null,
+                    name: 'test1',
+                    version: 0
+                },
+                {
+                    prevVersion: 1,
+                    name: 'test1',
+                    version: 2
+                }
+            ]
+        }).then(function (data) {
+            data.update.length.should.be.equal(1);
+            data.all.length.should.be.equal(2);
+            (num === 4).should.be.true();
+            done();
+        });
+    });
+
+    it('_sendLog update:0', done => {
+        let num = 0;
+        log._write = () => {
+            num += 1;
+        };
+
+        Check._sendLog({}, {
+            update: [
+            ],
+            all: [
+                {
+                    prevVersion: 1,
+                    name: 'test1',
+                    version: 1
+                }
+            ]
+        }).then(function (data) {
+            data.update.length.should.be.equal(0);
+            data.all.length.should.be.equal(1);
+            (num === 3).should.be.true();
+            done();
+        });
+    });
 });
